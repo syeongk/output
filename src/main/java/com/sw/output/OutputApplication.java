1e139cbe71104aff400d0b28c98bb4691cf36cd7
김서영 <syeongk7@gmail.com>
2025-04-28 00:25:24 +0900

    Initial commit

---
+++ src/main/java/com/sw/output/OutputApplication.java
@@ -0,0 +1,13 @@
+package com.sw.output;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutputApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sw/output/OutputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.output;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
