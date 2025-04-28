3bddf2a97763760d2bbb1c28e014bf7cf1ce1751
김서영 <syeongk7@gmail.com>
2025-04-28 16:45:19 +0900

    refactor : API 공통 응답 포맷

--- src/main/java/com/sw/output/global/response/errorcode/CommonErrorCode.java
+++ src/main/java/com/sw/output/global/response/errorcode/CommonErrorCode.java
@@ -1,13 +1,11 @@
 package com.sw.output.global.response.errorcode;
 
 import com.sw.output.global.response.BaseCode;
-import com.sw.output.global.response.errorcode.dto.ErrorReasonDTO;
 import org.springframework.http.HttpStatus;
 
 public enum CommonErrorCode implements BaseCode {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 에러입니다. 관리자에게 문의해 주세요.")
-    ;
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 에러입니다. 관리자에게 문의해 주세요.");
 
     private final HttpStatus httpStatus;
     private final String code;
@@ -32,22 +30,5 @@ public enum CommonErrorCode implements BaseCode {
     @Override
     public String getMessage() {
         return message;
-    }
-
-    public ErrorReasonDTO getReason() {
-        return ErrorReasonDTO.builder()
-                .code(code)
-                .message(message)
-                .isSuccess(false)
-                .build();
-    }
-
-    public ErrorReasonDTO getHttpStatusReason() {
-        return ErrorReasonDTO.builder()
-                .httpStatus(httpStatus)
-                .code(code)
-                .message(message)
-                .isSuccess(false)
-                .build();
     }
 }
