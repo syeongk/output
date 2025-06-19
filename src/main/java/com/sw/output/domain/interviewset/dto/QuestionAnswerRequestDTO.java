e15015c79a73d8fc42309c39f7662b95defe95d2
김서영 <syeongk7@gmail.com>
2025-06-19 11:23:36 +0900

    feat :질문 제목 사이즈 수정

--- src/main/java/com/sw/output/domain/interviewset/dto/QuestionAnswerRequestDTO.java
+++ src/main/java/com/sw/output/domain/interviewset/dto/QuestionAnswerRequestDTO.java
@@ -12,7 +12,7 @@ public class QuestionAnswerRequestDTO {
     @Builder
     public static class QuestionAnswerCreateDTO {
         @NotBlank(message = "질문 제목은 필수 항목입니다")
-        @Size(min = 1, max = 100, message = "질문 제목은 1~100자 사이여야 합니다")
+        @Size(min = 1, max = 200, message = "질문 제목은 1~200자 사이여야 합니다")
         private String questionTitle;
 
         private String answerContent;
