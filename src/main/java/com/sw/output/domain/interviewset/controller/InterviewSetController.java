e3b10c38e040ade40992353b3cf691778d8602be
김서영 <syeongk7@gmail.com>
2025-06-20 14:02:27 +0900

    docs : Swagger 명세 수정

--- src/main/java/com/sw/output/domain/interviewset/controller/InterviewSetController.java
+++ src/main/java/com/sw/output/domain/interviewset/controller/InterviewSetController.java
@@ -1,15 +1,5 @@
 package com.sw.output.domain.interviewset.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.sw.output.domain.interviewset.dto.InterviewSetRequestDTO;
 import com.sw.output.domain.interviewset.dto.InterviewSetResponseDTO;
 import com.sw.output.domain.interviewset.entity.InterviewCategory;
@@ -20,11 +10,11 @@ import com.sw.output.domain.interviewset.service.BookmarkService;
 import com.sw.output.domain.interviewset.service.InterviewSetService;
 import com.sw.output.global.dto.CommonResponseDTO;
 import com.sw.output.global.response.ApiResponse;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/interview-sets")
@@ -98,7 +88,7 @@ public class InterviewSetController {
     }
 
     @PostMapping("ai-questions")
-    @Operation(summary = "[개발중] AI 질문 생성 API", description = "면접 카테고리, 직무 카테고리, 면접 세트 제목에 대한 AI 질문 생성")
+    @Operation(summary = "AI 질문 생성 API", description = "면접 카테고리, 직무 카테고리, 면접 세트 제목에 대한 AI 질문 생성")
     public ApiResponse<InterviewSetResponseDTO.GetQuestionsDTO> createAiQuestions(
             @RequestBody @Valid InterviewSetRequestDTO.CreateQuestionsPromptDTO request) {
         InterviewSetResponseDTO.GetQuestionsDTO response = interviewSetService.createAiQuestions(request);
